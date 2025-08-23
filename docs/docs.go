6268781f31e820dca115598a9786c02c7ae0f6ab
Alec M. Wantoch <alec@wantoch.com>
2025-08-23 12:11:37 -0700

    add back comprehensive BEEMFLOW.md
    Signed-off-by: Alec M. Wantoch <alec@wantoch.com>

--- docs/docs.go
+++ docs/docs.go
@@ -2,16 +2,11 @@ package docs
 
 import _ "embed"
 
-// BeemflowSpec is the embedded BeemFlow spec document.
-//
-// This is the canonical, up-to-date protocol and config spec, including event bus config and schema references.
-//
 //go:embed SPEC.md
 var BeemflowSpec string
 
-// BeemflowComprehensive is the ultimate comprehensive BeemFlow documentation.
-// Currently disabled until BEEMFLOW.md is created.
-// var BeemflowComprehensive string
+//go:embed BEEMFLOW.md
+var BeemflowComprehensive string
 
 //go:embed beemflow.schema.json
 var BeemflowSchema string
