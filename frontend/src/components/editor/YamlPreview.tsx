1f3c665effb4cb0167914a0f315e7a921c981f6c
jiyuu-jin <zach@valist.io>
2025-10-25 03:44:30 -0400

    Fix yaml preview extra scroll bar

--- frontend/src/components/editor/YamlPreview.tsx
+++ frontend/src/components/editor/YamlPreview.tsx
@@ -154,7 +154,7 @@ export function YamlPreview({
       )}
 
       {/* YAML Content */}
-      <div className="flex-1 overflow-auto">
+      <div className="flex-1">
         <textarea
           value={yamlContent}
           onChange={(e) => {
